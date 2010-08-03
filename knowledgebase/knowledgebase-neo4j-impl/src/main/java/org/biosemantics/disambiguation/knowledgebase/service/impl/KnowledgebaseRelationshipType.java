bb34ac7d6ff9f121d70ec9c44b0c8c3907e3b526
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-08-03 16:51:10 +0000

    updated package hierarchy
    git-svn-id: https://transmog.googlecode.com/svn/trunk@22 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- knowledgebase/knowledgebase-neo4j-impl/src/main/java/org/biosemantics/disambiguation/knowledgebase/service/impl/KnowledgebaseRelationshipType.java
+++
@@ -1,8 +0,0 @@
-package org.biosemantics.disambiguation.knowledgebase.neo4j.impl;
-
-import org.neo4j.graphdb.RelationshipType;
-
-public enum KnowledgebaseRelationshipType implements RelationshipType{
-	LABELS, LABEL, CONCEPTS, CONCEPT, HAS_LABEL, NOTATIONS, NOTATION, HAS_NOTATION
-
-}
