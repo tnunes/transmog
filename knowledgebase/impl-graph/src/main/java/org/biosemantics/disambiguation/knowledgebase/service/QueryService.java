902d76e61ec4c052d366b762b14b5ef1bd8f4897
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-08-19 11:13:19 +0000

    Added new methods for API. Tests pending.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@60 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- knowledgebase/impl-graph/src/main/java/org/biosemantics/disambiguation/knowledgebase/service/QueryService.java
+++ knowledgebase/impl-graph/src/main/java/org/biosemantics/disambiguation/knowledgebase/service/QueryService.java
@@ -5,5 +5,7 @@ import java.util.Collection;
 public interface QueryService {
 	Collection<Concept> getConceptsByNotation(Domain domain, String code);
 	Concept getConceptById(String id);
+	Collection<Concept> getConceptsByLabelText(String labelText);
+	Collection<Concept> getConceptsByFullTextSearch(String text, int maxResults);
 	
 }
--- knowledgebase/impl-graph/src/main/java/org/biosemantics/disambiguation/knowledgebase/service/impl/QueryServiceImpl.java
+++ knowledgebase/impl-graph/src/main/java/org/biosemantics/disambiguation/knowledgebase/service/impl/QueryServiceImpl.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang.StringUtils;
 import org.biosemantics.disambiguation.knowledgebase.service.Concept;
 import org.biosemantics.disambiguation.knowledgebase.service.Domain;
 import org.biosemantics.disambiguation.knowledgebase.service.KnowledgebaseRelationshipType;
+import org.biosemantics.disambiguation.knowledgebase.service.Label;
 import org.biosemantics.disambiguation.knowledgebase.service.QueryService;
 import org.biosemantics.disambiguation.knowledgebase.service.local.TextIndexService;
 import org.neo4j.graphdb.Direction;
@@ -42,11 +43,45 @@ public class QueryServiceImpl implements QueryService {
 
 	@Override
 	public Concept getConceptById(String id) {
-		if(StringUtils.isBlank(id))
+		if (StringUtils.isBlank(id))
 			throw new IllegalArgumentException("id cannot be blank");
 		Concept concept = textIndexService.getConceptById(id);
-		if(concept == null)
+		if (concept == null)
 			throw new IllegalArgumentException("No concept found with id " + id);
 		return concept;
+	}
+
+	@Override
+	public Collection<Concept> getConceptsByLabelText(String labelText) {
+		if (StringUtils.isBlank(labelText))
+			throw new IllegalArgumentException("labelText cannot be blank");
+		List<Concept> concepts = new ArrayList<Concept>();
+		Collection<Label> labels = textIndexService.getLabelsByText(labelText);
+		for (Label label : labels) {
+			LabelImpl labelImpl = (LabelImpl) label;
+			if (labelImpl != null) {
+				Traverser traverser = labelImpl.getUnderlyingNode().traverse(Order.BREADTH_FIRST,
+						StopEvaluator.DEPTH_ONE, ReturnableEvaluator.ALL_BUT_START_NODE,
+						KnowledgebaseRelationshipType.HAS_LABEL, Direction.INCOMING);
+				for (Node conceptNode : traverser) {
+					concepts.add(new ConceptImpl(conceptNode));
+				}
+			}
+		}
+		return concepts;
+	}
+
+	@Override
+	public Collection<Concept> getConceptsByFullTextSearch(String text, int maxResults) {
+		if (StringUtils.isBlank(text))
+			throw new IllegalArgumentException("text cannot be blank");
+		if (maxResults <= 0)
+			throw new IllegalArgumentException("maxResults cannot be zero or a negative value");
+		Collection<Concept> concepts = textIndexService.fullTextSearch(text, maxResults);
+		if (concepts == null) {
+			return new ArrayList<Concept>();
+		} else {
+			return concepts;
+		}
 	}
 }
