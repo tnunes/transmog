b6c8a73913faf6904d790473c6ee316e2d5ae7c6
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-10-26 07:30:29 +0000

    Removed comment.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@87 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- disambiguation/conceptstore-impl/src/main/java/org/biosemantics/disambiguation/service/Index.java
+++ disambiguation/conceptstore-impl/src/main/java/org/biosemantics/disambiguation/service/Index.java
@@ -1,7 +1,6 @@
 package org.biosemantics.disambiguation.service;
 
 public abstract class Index {
-	// FIXME am i slipping implementation details here?
 	public static final String CONCEPT_FULL_TXT_INDEX = "concept_full_text";
 	public static final String LABEL_TXT_INDEX = "label_text";
 	public static final String NOTATION_CODE_INDEX = "notation_code";
