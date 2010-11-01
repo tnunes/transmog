80cd3cbb1a1e44677c49dec115f2a0a580fe9702
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-11-01 14:53:56 +0000

    Merged API's into one using ConceptDetails Data Transfer Object.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@104 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- datasource/datasource-common/src/main/java/org/biosemantics/datasource/common/DataSourceWriter.java
+++ datasource/datasource-common/src/main/java/org/biosemantics/datasource/common/DataSourceWriter.java
@@ -1,13 +1,10 @@
 package org.biosemantics.datasource.common;
 
-import org.biosemantics.conceptstore.common.domain.Concept;
 import org.biosemantics.conceptstore.common.domain.Relationship;
 
 public interface DataSourceWriter {
-	boolean writeConcept(Concept concept);
-	boolean writePredicate(Concept concept);
-	boolean writeConceptScheme(Concept concept);
-	boolean writeDomain(Concept concept);
+	boolean writeConcept(ConceptDetail conceptDetail);
+	
 	boolean writeRelationship(Relationship relationship);
 	
 }
