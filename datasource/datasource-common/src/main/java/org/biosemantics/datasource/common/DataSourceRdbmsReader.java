059ca1317fdd7002e442ad52e87da0746454e60d
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-11-05 09:17:18 +0000

    moved to main-disambiguation-project
    git-svn-id: https://transmog.googlecode.com/svn/trunk@108 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- datasource/datasource-common/src/main/java/org/biosemantics/datasource/common/DataSourceRdbmsReader.java
+++
@@ -1,9 +0,0 @@
-package org.biosemantics.datasource.common;
-
-import javax.sql.DataSource;
-
-public interface DataSourceRdbmsReader extends DataSourceReader {
-	
-	void setDataSource(DataSource dataSource);
-
-}
--- datasource/datasource-common/src/main/java/org/biosemantics/datasource/common/DataSourceSparqlReader.java
+++
@@ -1,7 +0,0 @@
-package org.biosemantics.datasource.common;
-
-import java.net.URI;
-
-public interface DataSourceSparqlReader extends DataSourceReader {
-	void setSparqlURI(URI uri);
-}
