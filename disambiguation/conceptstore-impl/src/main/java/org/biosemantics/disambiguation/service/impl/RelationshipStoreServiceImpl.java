84b45a405ab5fb56edcec78ba1e9a38f0c5530e9
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-11-23 13:41:30 +0000

    Implemented methods for the updated API.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@152 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- disambiguation/conceptstore-impl/src/main/java/org/biosemantics/disambiguation/service/impl/RelationshipStoreServiceImpl.java
+++ disambiguation/conceptstore-impl/src/main/java/org/biosemantics/disambiguation/service/impl/RelationshipStoreServiceImpl.java
@@ -2,20 +2,37 @@ package org.biosemantics.disambiguation.service.impl;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.biosemantics.conceptstore.common.domain.Concept;
 import org.biosemantics.conceptstore.common.domain.Relationship;
 import org.biosemantics.conceptstore.common.service.RelationshipStoreService;
 import org.biosemantics.conceptstore.utils.service.UuidGeneratorService;
 import org.biosemantics.disambiguation.domain.impl.ConceptImpl;
 import org.biosemantics.disambiguation.domain.impl.RelationshipImpl;
+import org.biosemantics.disambiguation.service.IndexService;
 import org.neo4j.graphdb.Direction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
 
 public class RelationshipStoreServiceImpl implements RelationshipStoreService {
 
+	private static final Logger logger = LoggerFactory.getLogger(RelationshipStoreServiceImpl.class);
 	private UuidGeneratorService uuidGeneratorService;
-	private boolean checkExists;
+	private IndexService indexService;
+	private boolean checkExists = true;
 
+	@Required
 	public void setUuidGeneratorService(UuidGeneratorService uuidGeneratorService) {
 		this.uuidGeneratorService = checkNotNull(uuidGeneratorService);
+	}
+
+	@Required
+	public void setIndexService(IndexService indexService) {
+		this.indexService = indexService;
 	}
 
 	public void setCheckExists(boolean checkExists) {
@@ -68,5 +85,29 @@ public class RelationshipStoreServiceImpl implements RelationshipStoreService {
 		}
 		return found;
 
+	}
+
+	@Override
+	public Relationship getRelationshipByUuid(String uuid) {
+		throw new UnsupportedOperationException(
+				"getRelationshipByUuid not supported yet, will be supported when we move to neo4j 1.2");
+	}
+
+	@Override
+	public Collection<Relationship> getAllRelationshipsForConcept(String uuid) {
+		Concept concept = indexService.getConceptByUuid(uuid);
+		if (concept == null) {
+			logger.warn("no concept in store for uuid {}", uuid);
+		}
+		ConceptImpl conceptImpl = (ConceptImpl) concept;
+		Iterable<org.neo4j.graphdb.Relationship> storeRelationships = conceptImpl.getUnderlyingNode().getRelationships(
+				ConceptRelationshipTypeImpl.CLOSE_MATCH, ConceptRelationshipTypeImpl.EXACT_MATCH,
+				ConceptRelationshipTypeImpl.HAS_BROADER_CONCEPT, ConceptRelationshipTypeImpl.HAS_NARROWER_CONCEPT,
+				ConceptRelationshipTypeImpl.RELATED);
+		Set<Relationship> relationships = new HashSet<Relationship>();
+		for (org.neo4j.graphdb.Relationship storeRelationship : storeRelationships) {
+			relationships.add(new RelationshipImpl(storeRelationship));
+		}
+		return relationships;
 	}
 }
