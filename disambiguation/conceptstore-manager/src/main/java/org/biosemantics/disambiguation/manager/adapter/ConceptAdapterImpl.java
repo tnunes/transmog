73497c58cfad0a20e103fd0cbd1fe7b4fef80155
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-12-01 14:28:55 +0000

    Temporary logging added for performance comparison
    git-svn-id: https://transmog.googlecode.com/svn/trunk@163 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- disambiguation/conceptstore-manager/src/main/java/org/biosemantics/disambiguation/manager/adapter/ConceptAdapterImpl.java
+++ disambiguation/conceptstore-manager/src/main/java/org/biosemantics/disambiguation/manager/adapter/ConceptAdapterImpl.java
@@ -1,6 +1,7 @@
 package org.biosemantics.disambiguation.manager.adapter;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.biosemantics.conceptstore.common.domain.Concept;
@@ -11,11 +12,15 @@ import org.biosemantics.disambiguation.manager.common.CommonUtility;
 import org.biosemantics.disambiguation.manager.dto.ConceptResult;
 import org.biosemantics.disambiguation.manager.dto.LabelResult;
 import org.biosemantics.disambiguation.manager.dto.NotationResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 
 public class ConceptAdapterImpl implements ConceptAdapter {
-
+	private static final Logger logger = LoggerFactory.getLogger(ConceptAdapterImpl.class);
 	@Override
 	public ConceptResult adapt(Concept concept) {
+
 		List<LabelResult> labels = new ArrayList<LabelResult>();
 		List<NotationResult> notations = new ArrayList<NotationResult>();
 		for (Label label : concept.getLabelsByType(LabelType.PREFERRED)) {
@@ -24,12 +29,14 @@ public class ConceptAdapterImpl implements ConceptAdapter {
 		for (Label label : concept.getLabelsByType(LabelType.ALTERNATE)) {
 			labels.add(adapt(label, LabelType.ALTERNATE));
 		}
-		if (concept.getNotations() != null && !concept.getNotations().isEmpty()) {
-			for (Notation notation : concept.getNotations()) {
-				notations.add(adapt(notation));
+		Collection<Notation> conceptNotations = concept.getNotations();
+		if (! CollectionUtils.isEmpty(conceptNotations)) {
+			for (Notation conceptNotation : conceptNotations) {
+				long start = System.currentTimeMillis();
+				notations.add(adapt(conceptNotation));
+				logger.info(" adapt for notations in {}:ms", (System.currentTimeMillis()-start));
 			}
 		}
-
 		return new ConceptResult(concept.getUuid(), CommonUtility.getPreferredLabel(
 				concept.getLabelsByType(LabelType.PREFERRED)).getText(), labels, notations);
 	}
@@ -39,8 +46,12 @@ public class ConceptAdapterImpl implements ConceptAdapter {
 	}
 
 	private NotationResult adapt(Notation notation) {
+		long start = System.currentTimeMillis();
 		Concept domain = notation.getDomain();
+		logger.info(" getDomain in {}:ms", (System.currentTimeMillis()-start));
+		start = System.currentTimeMillis();
 		Label preferredLabel = CommonUtility.getPreferredLabel(domain.getLabelsByType(LabelType.PREFERRED));
+		logger.info(" get Label in {}:ms", (System.currentTimeMillis()-start));
 		return new NotationResult(preferredLabel.getText(), preferredLabel.getLanguage(), notation.getCode());
 	}
 
