3d5f4ff748eda0adf3bb7086a995a60b6045234e
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2010-12-01 14:28:09 +0000

    Commented Relationship controller for now. Using AOP logging with logback. Configuration file for logging added.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@162 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- disambiguation/conceptstore-manager/src/main/java/org/biosemantics/disambiguation/manager/controller/RelationshipController.java
+++ disambiguation/conceptstore-manager/src/main/java/org/biosemantics/disambiguation/manager/controller/RelationshipController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+
 public class RelationshipController {
 	private static final Logger logger = LoggerFactory.getLogger(QueryController.class);
 	private RelationshipStoreService relationshipStoreService;
