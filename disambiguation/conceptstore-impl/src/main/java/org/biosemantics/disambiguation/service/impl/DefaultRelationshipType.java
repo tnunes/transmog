ead5fac06b36b4c18fa6ff1759fa8e5c37f2b417
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2011-04-12 13:32:49 +0000

    moved to local implementations.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@200 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- disambiguation/conceptstore-impl/src/main/java/org/biosemantics/disambiguation/service/impl/DefaultRelationshipType.java
+++
@@ -1,8 +0,0 @@
-package org.biosemantics.disambiguation.service.impl;
-
-import org.neo4j.graphdb.RelationshipType;
-
-public enum DefaultRelationshipType implements RelationshipType {
-	LABELS, LABEL, CONCEPTS, CONCEPT, NOTATIONS, NOTATION, HAS_NOTATION, PREDICATES, PREDICATE, CONCEPT_SCHEMES, TOP_CONCEPT, HAS_NOTE, NOTES, NOTE, DOMAINS, HAS_DOMAIN, HAS_LABEL,
-
-}
