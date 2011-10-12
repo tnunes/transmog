1c75e3d94decd038b106db14428a4f890fa969b5
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2011-10-12 09:33:36 +0000

    removed scripts. They will be relocated to research folder as they are more research specific.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@283 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- disambiguation/conceptstore-scripts/src/main/java/org/biosemantics/disambiguation/script/AmbiguousLabelScript.java
+++
@@ -1,9 +0,0 @@
-package org.biosemantics.disambiguation.script;
-
-public interface AmbiguousLabelScript {
-
-	void setOutputSink(OutputSink outputSink);
-
-	void writeAmbiguousLabels();
-
-}
--- disambiguation/conceptstore-scripts/src/main/java/org/biosemantics/disambiguation/script/WritableObject.java
+++
@@ -1,7 +0,0 @@
-package org.biosemantics.disambiguation.script;
-
-public interface WritableObject {
-
-	String[] toStringArray();
-
-}
