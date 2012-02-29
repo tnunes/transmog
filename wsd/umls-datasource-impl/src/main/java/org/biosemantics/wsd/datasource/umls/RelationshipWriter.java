0eb2ed3e530457d04f3339dd131af34556621698
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2012-02-29 13:45:55 +0000

    smaller transactions for importing relationships.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@297 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/RelationshipWriter.java
+++ wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/RelationshipWriter.java
@@ -11,6 +11,9 @@ import javax.sql.DataSource;
 
 import org.biosemantics.wsd.domain.Concept;
 import org.biosemantics.wsd.repository.ConceptRepository;
+import org.neo4j.graphdb.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,10 +26,10 @@ public class RelationshipWriter {
 	private ConceptRepository conceptRepository;
 	@Autowired
 	private Neo4jTemplate neo4jTemplate;
-	private static final String GET_ALL_FACTUAL_RLSP_SQL = "select CUI1, CUI2, REL from MRREL where CUI1 != CUI2 AND REL IN ('CHD', 'RB', 'RO', 'SIB', 'SY')";// 14953305
+	private static final String GET_ALL_FACTUAL_RLSP_SQL = "select CUI1, CUI2, REL, RELA from MRREL where CUI1 != CUI2 AND REL IN ('CHD', 'RB', 'RO', 'SIB', 'SY')";// 14953305
+	private static final Logger logger = LoggerFactory.getLogger(RelationshipWriter.class);
 	private Map<String, String> cache = new HashMap<String, String>();
 
-	@Transactional
 	public void writeAll() throws SQLException {
 		Connection connection = dataSource.getConnection();
 		Statement stmt = connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
@@ -34,18 +37,25 @@ public class RelationshipWriter {
 		stmt.setFetchSize(Integer.MIN_VALUE);
 		try {
 			ResultSet rs = stmt.executeQuery(GET_ALL_FACTUAL_RLSP_SQL);
+			int ctr = 0;
+			Transaction tx = neo4jTemplate.beginTx();
 			while (rs.next()) {
 				String cui1 = rs.getString("CUI1");
 				String cui2 = rs.getString("CUI2");
 				String rel = rs.getString("REL");
 				String rela = rs.getString("RELA");
 				if (cache.containsKey(cui1 + cui2) || cache.containsKey(cui2 + cui1)) {
+					// rlsp exists do nothing
+				} else {
 					String relUpper = rel.toUpperCase().trim();
 					if (relUpper.equals("CHD") || relUpper.equals("RB") || relUpper.equals("RO")
 							|| relUpper.equals("SIB") || relUpper.equals("SY")) {
-						// Find all relationships for a UMLS concept. Note: In MRREL, the REL/RELA always expresses the
-						// nature of the relationship from CUI2 to the "current concept", CUI1. Because we're querying
-						// CUI1 below, this represents the "natural" direction of the relationship.
+						// Find all relationships for a UMLS concept. Note: In
+						// MRREL, the REL/RELA always expresses the
+						// nature of the relationship from CUI2 to the
+						// "current concept", CUI1. Because we're querying
+						// CUI1 below, this represents the "natural" direction
+						// of the relationship.
 						if (relUpper.equals("CHD")) {
 							Concept concept = conceptRepository.getConceptById(cui2);
 							Concept otherConcept = conceptRepository.getConceptById(cui1);
@@ -60,11 +70,19 @@ public class RelationshipWriter {
 							concept.relatedTo(neo4jTemplate, otherConcept, 100, rela, UMLS_2011_AA);
 						}
 					} else {
-						// logger.error("REL  = {} received. Illegal value for relationships.", rel);
+						// logger.error("REL  = {} received. Illegal value for relationships.",
+						// rel);
 						throw new IllegalArgumentException("Illegal value for relationships." + rel);
 					}
-				} else {
+					// add to cache for future recognition
 					cache.put(cui1 + cui2, null);
+				}
+				// 1 million
+				if (++ctr % 1000000 == 0) {
+					tx.success();
+					tx.finish();
+					tx = neo4jTemplate.beginTx();
+					logger.info("ctr:{}", ctr);
 				}
 			}
 			rs.close();
--- wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/UmlsDbToGraphWriter.java
+++ wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/UmlsDbToGraphWriter.java
@@ -2,21 +2,24 @@ package org.biosemantics.wsd.datasource.umls;
 
 import java.sql.SQLException;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UmlsDbToGraphWriter {
 
-	private ApplicationContext applicationContext;
+	private static ClassPathXmlApplicationContext applicationContext;
 
-	public UmlsDbToGraphWriter() throws SQLException {
+	public static void main(String[] args) throws SQLException {
 		applicationContext = new ClassPathXmlApplicationContext(new String[] { "umls-import-context.xml" });
 		ConceptNodeWriter conceptNodeWriter = applicationContext.getBean(ConceptNodeWriter.class);
 		conceptNodeWriter.writeAll();
-	}
+		applicationContext.close();
+		//hopefully release memory
+		applicationContext = new ClassPathXmlApplicationContext(new String[] { "umls-import-context.xml" });
+		applicationContext.registerShutdownHook();
+		RelationshipWriter relationshipWriter = applicationContext.getBean(RelationshipWriter.class);
+		relationshipWriter.writeAll();
+		applicationContext.close();
 
-	public static void main(String[] args) throws SQLException {
-		UmlsDbToGraphWriter obj = new UmlsDbToGraphWriter();
 	}
 
 }
