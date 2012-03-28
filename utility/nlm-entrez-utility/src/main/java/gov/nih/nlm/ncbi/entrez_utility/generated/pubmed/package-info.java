4f6bbff82077117b39a4c0399a11251e9a140980
bhanu1313@gmail.com <bhanu1313@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2012-03-28 15:36:32 +0000

    removed generated files.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@327 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- utility/nlm-entrez-utility/src/main/java/gov/nih/nlm/ncbi/entrez_utility/generated/pubmed/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ncbi.nlm.nih.gov/soap/eutils/efetch_pubmed", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.nih.nlm.ncbi.entrez_utility.generated.pubmed;
