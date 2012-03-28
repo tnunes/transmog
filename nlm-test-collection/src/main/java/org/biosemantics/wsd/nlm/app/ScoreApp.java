b151342a361edc491aa75f9446cd3d4af69bfef7
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2012-03-28 15:48:13 +0000

    added updated scripts and ignored files
    git-svn-id: https://transmog.googlecode.com/svn/trunk@330 faf811b6-6cfc-abfb-8d20-f6923ad738d4

---
+++ nlm-test-collection/src/main/java/org/biosemantics/wsd/nlm/app/ScoreApp.java
@@ -0,0 +1,41 @@
+package org.biosemantics.wsd.nlm.app;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+public class ScoreApp {
+
+	public static final String RESULT_FOLDER = "/ssd/bhsingh/data/pseudocode-score";
+	public static final String OUT_FOLDER = "/ssd/bhsingh/data/";
+
+	public static void main(String[] args) throws IOException {
+		File folder = new File(RESULT_FOLDER);
+		CSVWriter csvWriter = new CSVWriter(new FileWriter(new File(OUT_FOLDER, "01_summary.txt")));
+		File[] files = folder.listFiles();
+		for (File file : files) {
+			CSVReader csvReader = new CSVReader(new FileReader(file));
+			List<String[]> lines = csvReader.readAll();
+			double accuracy = 0;
+			for (String[] columns : lines) {
+				String value = columns[columns.length - 1];
+				if (Boolean.valueOf(value)) {
+					accuracy++;
+				}
+			}
+			double accuracyPercentage = (accuracy / lines.size())*100;
+			csvWriter.writeNext(new String[] { file.getName(), "" + accuracy, "" + lines.size(),
+					"" + accuracyPercentage });
+			csvReader.close();
+
+		}
+		csvWriter.flush();
+		csvWriter.close();
+	}
+
+}
