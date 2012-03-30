9232869530e0e1d066e845784f72ab3f48aaef7f
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2012-03-30 09:46:48 +0000

    Added shutdown hook, removed closing reopening context as it does not affect memory handling.
    git-svn-id: https://transmog.googlecode.com/svn/trunk@333 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/UmlsDbToGraphWriter.java
+++ wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/UmlsDbToGraphWriter.java
@@ -10,16 +10,12 @@ public class UmlsDbToGraphWriter {
 
 	public static void main(String[] args) throws SQLException {
 		applicationContext = new ClassPathXmlApplicationContext(new String[] { "umls-import-context.xml" });
+		applicationContext.registerShutdownHook();
 		ConceptNodeWriter conceptNodeWriter = applicationContext.getBean(ConceptNodeWriter.class);
 		conceptNodeWriter.writeAll();
-		applicationContext.close();
-		//hopefully release memory
-		applicationContext = new ClassPathXmlApplicationContext(new String[] { "umls-import-context.xml" });
-		applicationContext.registerShutdownHook();
 		RelationshipWriter relationshipWriter = applicationContext.getBean(RelationshipWriter.class);
 		relationshipWriter.writeAll();
 		applicationContext.close();
-
 	}
 
 }
