5bbb7ea62e351259d9ceba291149095715501e4a
bsbhadwal@gmail.com <bsbhadwal@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2012-05-03 15:51:30 +0000

    Added 2 methods. getPreferredLabel and getConceptScheme
    git-svn-id: https://transmog.googlecode.com/svn/trunk@359 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- wsd/conceptstore-graph-impl/src/main/java/org/biosemantics/wsd/repository/ConceptRepository.java
+++ wsd/conceptstore-graph-impl/src/main/java/org/biosemantics/wsd/repository/ConceptRepository.java
@@ -1,6 +1,7 @@
 package org.biosemantics.wsd.repository;
 
 import org.biosemantics.wsd.domain.Concept;
+import org.biosemantics.wsd.domain.Label;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 
@@ -10,5 +11,11 @@ public interface ConceptRepository extends GraphRepository<Concept> {
 	
 	@Query("start concept=node({0}) match concept-[:RELATED]-otherConcept return otherConcept")
 	Iterable<Concept> getRelatedConcepts(Concept concept);
+	
+	@Query("start concept=node({0}) match concept-[r:HAS_LABEL]-label where r.type = \"PREFERRED\" and label.language = {1} return label ")
+	Label getPreferredLabel(Concept concept, String language);
+	
+	@Query("start concept=node({0}) match concept-[:IN_SCHEME]-otherConcept return otherConcept")
+	Iterable<Concept> getConceptSchemes(Concept concept);
 	
 }
