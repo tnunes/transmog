6a344be68a9133cb89bde5c1997db476e49f2de4
bhanu1313@gmail.com <bhanu1313@gmail.com@faf811b6-6cfc-abfb-8d20-f6923ad738d4>
2012-05-30 12:50:53 +0000

    Added new scripts
    git-svn-id: https://transmog.googlecode.com/svn/trunk@362 faf811b6-6cfc-abfb-8d20-f6923ad738d4

--- wsd/wsd-script-impl/src/main/java/org/biosemantics/wsd/script/ScriptApp.java
+++ wsd/wsd-script-impl/src/main/java/org/biosemantics/wsd/script/ScriptApp.java
@@ -1,18 +1,22 @@
 package org.biosemantics.wsd.script;
 
+import java.io.IOException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ScriptApp {
 	
 	private static final String[] CONTEXT = new String[]{"script-run-context.xml"};
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(CONTEXT);
 		applicationContext.registerShutdownHook();
 //		AmbiguousTermFrequencyGenerator ambiguousTermFrequencyGenerator = applicationContext.getBean(AmbiguousTermFrequencyGenerator.class);
 //		ambiguousTermFrequencyGenerator.writeAll();
-		ConnectedConceptFrequencyGenerator conceptFrequencyGenerator = applicationContext.getBean(ConnectedConceptFrequencyGenerator.class);
-		conceptFrequencyGenerator.writeAll();
+//		MaxRelatedConceptGenerator maxRelatedConceptGenerator = applicationContext.getBean(MaxRelatedConceptGenerator.class);
+//		maxRelatedConceptGenerator.writeAll();
+		RelatedConceptWriter relatedConceptWriter = applicationContext.getBean(RelatedConceptWriter.class);
+		relatedConceptWriter.writeAll();
 	}
 
 }
