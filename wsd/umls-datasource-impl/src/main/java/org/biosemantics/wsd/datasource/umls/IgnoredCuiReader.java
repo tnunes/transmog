b379281dce09218987f4329a9eb994dced0075f9
Bharat Singh <singh.bharat@live.com>
2012-08-02 11:52:04 +0200

    Added ignored cui implementation to ignore non relevant cuis in the graph

---
+++ wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/IgnoredCuiReader.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.biosemantics.wsd.datasource.umls;
+
+/**
+ *
+ * @author bhsingh
+ */
+package org.biosemantics.wsd.datasource.umls;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+public class IgnoredCuiReader {
+
+	Map<String, Object> ignoredCuisMap = new HashMap<String, Object>();
+
+	public void init() throws IOException {
+		InputStream is = this.getClass().getClassLoader()
+				.getResourceAsStream("org/biosemantics/wsd/datasource/umls/ignore.txt");
+		if (is == null) {
+			logger.info("no ignore.txt file found");
+		} else {
+			CSVReader csvReader = new CSVReader(new InputStreamReader(is));
+			List<String[]> lines = csvReader.readAll();
+			for (String[] columns : lines) {
+				ignoredCuisMap.put(columns[0].trim(), null);
+			}
+			logger.info("{} cuis ignored.", ignoredCuisMap.size());
+			csvReader.close();
+		}
+	}
+
+	public boolean isIgnored(String cui) {
+		return ignoredCuisMap.containsKey(cui);
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(IgnoredCuiReader.class);
+	
+	
+	public static void main(String[] args) throws IOException {
+		IgnoredCuiReader ignoredCuiReader = new IgnoredCuiReader();
+		ignoredCuiReader.init();
+	}
+
+}
