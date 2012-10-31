f51c329189cde0a7eca5e17db6f89c7876b396fb
Bharat Singh <singh.bharat@live.com>
2012-10-31 12:08:36 +0100

    removed all other projects that were sharing the transmog repository. Will have individual repositories for such projects. Updated to Neo4j 1.8
    removed all other projects that were sharing the transmog repository.
    Will have individual repositories for such projects.
    Updated to Neo4j 1.8
    Added Lodd datasources.

--- research/nlm-wsd/src/main/java/org/biosemantics/disambiguation/umls/wsd/SenseScoreService.java
+++
@@ -1,9 +0,0 @@
-package org.biosemantics.disambiguation.umls.wsd;
-
-
-public interface SenseScoreService {
-	
-	double getSenseScore(String cui, String contextCui);
-	
-	
-}
--- utility/misc-utility/src/main/java/org/biosemantics/misc/utility/herman/PublicationDate.java
+++
@@ -1,5 +0,0 @@
-package org.biosemantics.misc.utility.herman;
-
-public class PublicationDate {
-
-}
