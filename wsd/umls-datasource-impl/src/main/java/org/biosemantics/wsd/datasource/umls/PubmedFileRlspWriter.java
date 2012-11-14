e0ff16285537d9dcc63e4add89bbdfc1153a6a46
Bharat Singh <singh.bharat@live.com>
2012-11-14 14:22:17 +0100

    Updated with cached implementation

--- wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/PubmedFileRlspWriter.java
+++ wsd/umls-datasource-impl/src/main/java/org/biosemantics/wsd/datasource/umls/PubmedFileRlspWriter.java
@@ -12,12 +12,12 @@ import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.apache.commons.lang.StringUtils;
 import org.biosemantics.conceptstore.domain.Concept;
 import org.biosemantics.conceptstore.domain.ConceptType;
 import org.biosemantics.conceptstore.domain.Label;
 import org.biosemantics.conceptstore.domain.LabelType;
 import org.biosemantics.conceptstore.domain.Notation;
-import org.biosemantics.conceptstore.domain.NotationSourceConstant;
 import org.biosemantics.conceptstore.domain.RlspType;
 import org.biosemantics.conceptstore.repository.ConceptRepository;
 import org.biosemantics.conceptstore.repository.LabelRepository;
@@ -37,14 +37,112 @@ public class PubmedFileRlspWriter {
 
 	private File inputFile;
 
-	public void setInputFile(String inputFile) {
-		this.inputFile = new File(inputFile);
-		if (!this.inputFile.exists()) {
-			throw new IllegalStateException("input file does not exist");
+	public void writeRlspsBetweenConceptsFromCsvFile(String file) throws IOException {
+		CSVReader reader = new CSVReader(new FileReader(file));
+		String[] nextLine;
+		Transaction tx = template.getGraphDatabaseService().beginTx();
+		int ctr = 0;
+		int totalLines = 0;
+		while ((nextLine = reader.readNext()) != null) {
+			if (nextLine.length == 4) {
+				if (++totalLines % txSize == 0) {
+					System.out.println("millis:" + System.currentTimeMillis() + " lines:" + totalLines);
+				}
+				String rela = nextLine[3].trim();
+				String rel = nextLine[2].trim();
+				boolean useRel = false;
+				if (StringUtils.isBlank(rela) || rela.equalsIgnoreCase("N")) {
+					useRel = true;
+				}
+				// rlsps in mrrel are CUI2 to CUI1
+				String sourceCui = nextLine[1].trim();
+				String targetCui = nextLine[0].trim();
+				if (ignoredCuiReader.isIgnored(sourceCui) || ignoredCuiReader.isIgnored(targetCui)) {
+					continue;
+				}
+				Long predNodeId = null;
+				if (useRel) {
+					if (predicateNodeIdMap.containsKey(rel)) {
+						predNodeId = predicateNodeIdMap.get(rel);
+					} else {
+						Notation predNotation = notationRepository.findByPropertyValue("code", rel);
+						if (predNotation != null) {
+							for (Concept concept : predNotation.getRelatedConcepts()) {
+								if (concept.getType() == ConceptType.PREDICATE) {
+									predNodeId = concept.getNodeId();
+									predicateNodeIdMap.put(rel, predNodeId);
+									break;
+								}
+							}
+						}
+					}
+				} else {
+					if (predicateNodeIdMap.containsKey(rela)) {
+						predNodeId = predicateNodeIdMap.get(rela);
+					} else {
+						Label predLabel = labelRepository.findByPropertyValue("text", rela);
+						if (predLabel != null) {
+							for (Concept concept : predLabel.getRelatedConcepts()) {
+								if (concept.getType() == ConceptType.PREDICATE) {
+									predNodeId = concept.getNodeId();
+									predicateNodeIdMap.put(rela, predNodeId);
+									break;
+								}
+							}
+						}
+					}
+				}
+				Long sourceNodeId = null;
+				if (cuiNodeIdMap.containsKey(sourceCui)) {
+					sourceNodeId = cuiNodeIdMap.get(sourceCui);
+				} else {
+					Notation srcNotation = notationRepository.findByPropertyValue("code", sourceCui);
+					if (srcNotation != null) {
+						for (Concept concept : srcNotation.getRelatedConcepts()) {
+							if (concept.getType() == ConceptType.CONCEPT) {
+								sourceNodeId = concept.getNodeId();
+								cuiNodeIdMap.put(sourceCui, sourceNodeId);
+								break;
+							}
+						}
+					}
+				}
+				Long targetNodeId = null;
+				if (cuiNodeIdMap.containsKey(targetCui)) {
+					targetNodeId = cuiNodeIdMap.get(targetCui);
+				} else {
+					Notation targetNotation = notationRepository.findByPropertyValue("code", targetCui);
+					if (targetNotation != null) {
+						for (Concept concept : targetNotation.getRelatedConcepts()) {
+							if (concept.getType() == ConceptType.CONCEPT) {
+								targetNodeId = concept.getNodeId();
+								cuiNodeIdMap.put(targetCui, targetNodeId);
+							}
+						}
+					}
+				}
+				if (sourceNodeId != null && targetNodeId != null && predNodeId != null) {
+					Concept srcConcept = conceptRepository.findOne(sourceNodeId);
+					Concept targetConcept = conceptRepository.findOne(targetNodeId);
+					srcConcept.addRelationshipIfNoBidirectionalRlspExists(template, targetConcept,
+							predNodeId.toString(), 0, MRREL);
+				} else {
+					System.err.println("sourceCui:" + sourceCui + " targetCui:" + targetCui + " rel" + rel + " rela:"
+							+ rela);
+				}
+				if (++ctr % txSize == 0) {
+					tx.success();
+					tx.finish();
+					tx = template.getGraphDatabaseService().beginTx();
+					System.out.println("millis:" + System.currentTimeMillis() + " ctr:" + ctr);
+				}
+			}
 		}
+		tx.success();
+		tx.finish();
 	}
 
-	public void addPubmedRelationships() throws IOException {
+	public void addPubmedRelationships(File inputFile) throws IOException {
 		LineIterator iterator = FileUtils.lineIterator(inputFile, "UTF-8");
 		System.out.println(System.currentTimeMillis() + " iterating file");
 		Transaction tx = template.getGraphDatabaseService().beginTx();
@@ -59,22 +157,29 @@ public class PubmedFileRlspWriter {
 					String tgtCui = columns[2].trim();
 					String pmid = columns[3].trim();
 					if (!ignoredCuiReader.isIgnored(srcCui) && !ignoredCuiReader.isIgnored(tgtCui)) {
-						Concept srcConcept = null;
-						Concept tgtConcept = null;
+						Long srcConceptNodeId = null;
+						Long tgtConceptNodeId = null;
 						Long predConceptNodeId = null;
-						Notation srcNotation = notationRepository.getNotation(NotationSourceConstant.UMLS.toString(),
-								srcCui);
-						if (srcNotation != null) {
-							for (Concept concept : srcNotation.getRelatedConcepts()) {
-								srcConcept = concept;
+						if (cuiNodeIdMap.containsKey(srcCui)) {
+							srcConceptNodeId = cuiNodeIdMap.get(srcCui);
+						} else {
+							Notation srcNotation = notationRepository.findByPropertyValue("code", srcCui);
+							if (srcNotation != null) {
+								for (Concept concept : srcNotation.getRelatedConcepts()) {
+									srcConceptNodeId = concept.getNodeId();
+									cuiNodeIdMap.put(srcCui, srcConceptNodeId);
+								}
 							}
 						}
-
-						Notation tgtNotation = notationRepository.getNotation(NotationSourceConstant.UMLS.toString(),
-								tgtCui);
-						if (tgtNotation != null) {
-							for (Concept concept : tgtNotation.getRelatedConcepts()) {
-								tgtConcept = concept;
+						if (cuiNodeIdMap.containsKey(tgtCui)) {
+							tgtConceptNodeId = cuiNodeIdMap.get(tgtCui);
+						} else {
+							Notation tgtNotation = notationRepository.findByPropertyValue("code", tgtCui);
+							if (tgtNotation != null) {
+								for (Concept concept : tgtNotation.getRelatedConcepts()) {
+									tgtConceptNodeId = concept.getNodeId();
+									cuiNodeIdMap.put(tgtCui, tgtConceptNodeId);
+								}
 							}
 						}
 						if (predicateNodeIdMap.containsKey(predicateText)) {
@@ -90,9 +195,10 @@ public class PubmedFileRlspWriter {
 								}
 							}
 						}
-						if (srcConcept != null && tgtConcept != null && predConceptNodeId != null) {
-							srcConcept.addRelationshipIfNoneExists(template, tgtConcept,
-									String.valueOf(predConceptNodeId), 0, "PMID:" + pmid);
+						if (srcConceptNodeId != null && tgtConceptNodeId != null && predConceptNodeId != null) {
+							(conceptRepository.findOne(srcConceptNodeId)).addRelationshipIfNoneExists(template,
+									(conceptRepository.findOne(tgtConceptNodeId)), String.valueOf(predConceptNodeId),
+									0, "PMID:" + pmid);
 							if (++ctr % txSize == 0) {
 								tx.success();
 								tx.finish();
@@ -100,8 +206,8 @@ public class PubmedFileRlspWriter {
 								System.out.println("millis:" + System.currentTimeMillis() + " ctr:" + ctr);
 							}
 						} else {
-							System.err.println("src:" + srcCui + ":" + srcConcept + " pred:" + predicateText + ":"
-									+ predConceptNodeId + " tgt:" + tgtCui + ":" + tgtConcept);
+							System.err.println("src:" + srcCui + ":" + srcConceptNodeId + " pred:" + predicateText
+									+ ":" + predConceptNodeId + " tgt:" + tgtCui + ":" + tgtConceptNodeId);
 						}
 					}
 				} else {
@@ -193,7 +299,10 @@ public class PubmedFileRlspWriter {
 	@Autowired
 	private ConceptRepository conceptRepository;
 	private Map<String, Long> predicateNodeIdMap = new HashMap<String, Long>();
+	private Map<String, Long> cuiNodeIdMap = new HashMap<String, Long>();
 
 	private static final int txSize = 1000;
+	private static final String UMLS_VERSION = "UMLS2012AA";
+	private static final String MRREL = UMLS_VERSION + "|MRREL";
 
 }
